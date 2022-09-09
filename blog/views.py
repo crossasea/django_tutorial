843240fc5756612c4c85eff4755025233a775858
crossasea <rrrroarrrrrr@gmail.com>
2022-09-09 12:56:45 +0530

    individual pages for posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 def home(request):
@@ -8,6 +9,16 @@ def home(request):
         'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
     
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})  
